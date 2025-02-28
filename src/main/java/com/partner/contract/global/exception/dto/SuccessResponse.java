8b265e42ecaa0a36c6ed8fc9713f3bdba48d9155
DAY0522 <ekdud_____@naver.com>
2025-02-28 16:12:09 +0900

    :sparkles: 성공/에러 응답 dto 코드 추가 (#6)

---
+++ src/main/java/com/partner/contract/global/exception/dto/SuccessResponse.java
@@ -0,0 +1,16 @@
+package com.partner.contract.global.exception.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SuccessResponse<T> {
+    private String code; // 응답 코드
+    private String message; // 응답 코드에 대한 상세 메세지
+    private T data; // 전달할 데이터
+
+    public static <T> SuccessResponse<T> of(String code, String message, T data) {
+        return new SuccessResponse<>(code, message, data);
+    }
+}
