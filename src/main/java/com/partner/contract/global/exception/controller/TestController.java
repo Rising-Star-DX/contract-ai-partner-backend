86ead3d78a7b3db73789b30dcfc18b7036b74b91
DAY0522 <ekdud_____@naver.com>
2025-03-10 02:12:10 +0900

    :sparkles: API 서버 정상 동작 확인을 위한 코드 추가 (#15)

--- src/main/java/com/partner/contract/global/exception/controller/TestController.java
+++ src/main/java/com/partner/contract/global/exception/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.partner.contract.global.exception.controller;
 
 import com.partner.contract.global.exception.dto.SuccessResponse;
+import com.partner.contract.global.exception.error.ApplicationException;
+import com.partner.contract.global.exception.error.ErrorCode;
 import com.partner.contract.global.exception.error.SuccessCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,5 +14,10 @@ public class TestController {
     @GetMapping("/success")
     public ResponseEntity<SuccessResponse<String>> success() {
         return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SELECT_SUCCESS, null));
+    }
+
+    @GetMapping("/fail")
+    public ResponseEntity<SuccessResponse<Void>> fail() {
+        throw new ApplicationException(ErrorCode.UNAUTHORIZED_ERROR);
     }
 }
