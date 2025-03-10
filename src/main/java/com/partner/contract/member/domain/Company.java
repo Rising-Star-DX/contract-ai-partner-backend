31f0d9f6a7ca2010ff17b722feacfd6befe59773
DAY0522 <ekdud_____@naver.com>
2025-03-10 16:17:18 +0900

    :sparkles: Company 엔티티 코드 추가 (#21)

---
+++ src/main/java/com/partner/contract/member/domain/Company.java
@@ -0,0 +1,36 @@
+package com.partner.contract.member.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
+    private List<Member> members;
+
+    @Builder
+    public Company(String name, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
