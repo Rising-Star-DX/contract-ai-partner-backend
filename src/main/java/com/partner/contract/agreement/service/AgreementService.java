eb10742c6df835313b1a389e1806e28994a7f4ab
minseokim0629 <han8121449@naver.com>
2025-03-12 12:10:22 +0900

    :bug: 네이밍 및 0개일 경우 에러가 아닌 빈 리스트를 반환하도록 수정 (#31)

--- src/main/java/com/partner/contract/agreement/service/AgreementService.java
+++ src/main/java/com/partner/contract/agreement/service/AgreementService.java
@@ -1,10 +1,7 @@
 package com.partner.contract.agreement.service;
 
-import com.partner.contract.agreement.domain.Agreement;
 import com.partner.contract.agreement.dto.AgreementResponseDto;
 import com.partner.contract.agreement.repository.AgreementRepository;
-import com.partner.contract.global.exception.error.ApplicationException;
-import com.partner.contract.global.exception.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,12 +15,10 @@ import java.util.stream.Collectors;
 public class AgreementService {
     private final AgreementRepository agreementRepository;
 
-    public List<AgreementResponseDto> getAllAgreements() {
-        List<Agreement> agreements = agreementRepository.findAll();
-        if(agreements.isEmpty()) {
-            throw new ApplicationException(ErrorCode.NOT_FOUND_ERROR);
-        }
-        return agreements.stream()
-                .map(AgreementResponseDto::fromEntity).collect(Collectors.toList());
+    public List<AgreementResponseDto> findAgreementList() {
+        return agreementRepository.findAllByOrderByCreatedAtDesc()
+                .stream()
+                .map(AgreementResponseDto::fromEntity)
+                .collect(Collectors.toList());
     }
 }
