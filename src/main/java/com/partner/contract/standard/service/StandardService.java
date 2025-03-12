93cf9d54bd6a17bc87976208ced4f0931539c9d7
minseokim0629 <han8121449@naver.com>
2025-03-12 12:10:42 +0900

    :bug: 네이밍 및 0개일 경우 에러가 아닌 빈 리스트를 반환하도록 수정 (#28)

--- src/main/java/com/partner/contract/standard/service/StandardService.java
+++ src/main/java/com/partner/contract/standard/service/StandardService.java
@@ -1,8 +1,5 @@
 package com.partner.contract.standard.service;
 
-import com.partner.contract.global.exception.error.ApplicationException;
-import com.partner.contract.global.exception.error.ErrorCode;
-import com.partner.contract.standard.domain.Standard;
 import com.partner.contract.standard.dto.StandardResponseDto;
 import com.partner.contract.standard.repository.StandardRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +15,10 @@ import java.util.stream.Collectors;
 public class StandardService {
     private final StandardRepository standardRepository;
 
-    public List<StandardResponseDto> getAllStandards() {
-        List<Standard> standards = standardRepository.findAll();
-        if(standards.isEmpty()) {
-            throw new ApplicationException(ErrorCode.NOT_FOUND_ERROR);
-        }
-        return standards.stream()
-                .map(StandardResponseDto::fromEntity).collect(Collectors.toList());
+    public List<StandardResponseDto> findStandardList() {
+        return standardRepository.findAllByOrderByCreatedAtDesc()
+                .stream()
+                .map(StandardResponseDto::fromEntity)
+                .collect(Collectors.toList());
     }
 }
