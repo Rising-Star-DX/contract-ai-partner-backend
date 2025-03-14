c3ac002de0bd3473b3eb58a5555dac508adabd0a
DAY0522 <ekdud_____@naver.com>
2025-03-14 16:12:29 +0900

    :sparkles: 파일 업로드 전 기준 문서 고유 번호 반환 API 개발 (#48)

--- src/main/java/com/partner/contract/standard/service/StandardService.java
+++ src/main/java/com/partner/contract/standard/service/StandardService.java
@@ -1,8 +1,11 @@
 package com.partner.contract.standard.service;
 
+import com.partner.contract.category.domain.Category;
+import com.partner.contract.category.repository.CategoryRepository;
 import com.partner.contract.global.exception.error.ApplicationException;
 import com.partner.contract.global.exception.error.ErrorCode;
 import com.partner.contract.standard.domain.Standard;
+import com.partner.contract.standard.dto.FileUploadInitRequestDto;
 import com.partner.contract.standard.dto.StandardListResponseDto;
 import com.partner.contract.standard.repository.StandardRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +20,7 @@ import java.util.stream.Collectors;
 @Transactional
 public class StandardService {
     private final StandardRepository standardRepository;
+    private final CategoryRepository categoryRepository;
 
     public List<StandardListResponseDto> findStandardList() {
         return standardRepository.findAllByOrderByCreatedAtDesc()
@@ -41,5 +45,18 @@ public class StandardService {
                 .stream()
                 .map(StandardListResponseDto::fromEntity)
                 .collect(Collectors.toList());
+    }
+
+    public Long initFileUpload(FileUploadInitRequestDto requestDto) {
+        Category category = categoryRepository.findById(requestDto.getCategoryId())
+                .orElseThrow(() -> new ApplicationException(ErrorCode.CATEGORY_NOT_FOUND_ERROR));
+
+        Standard standard = Standard.builder()
+                .name(requestDto.getName())
+                .type(requestDto.getType())
+                .category(category)
+                .build();
+
+        return standardRepository.save(standard).getId();
     }
 }
