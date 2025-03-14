237a0f242b78d1f8c99f3f93c0a028f4043824a8
minseokim0629 <han8121449@naver.com>
2025-03-14 17:37:04 +0900

    :bug: CORS 문제 해결 (localhost만 허용)

--- src/main/java/com/partner/contract/standard/controller/StandardController.java
+++ src/main/java/com/partner/contract/standard/controller/StandardController.java
@@ -15,6 +15,7 @@ import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/standards")
 public class StandardController {
     private final StandardService standardService;
