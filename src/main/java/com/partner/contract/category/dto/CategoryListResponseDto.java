481e0e36f67afd9a60902c380f582688482c9286
minseokim0629 <han8121449@naver.com>
2025-03-19 15:15:34 +0900

    :bug:  카테고리 리스트 dto 구조 수정 (#66)

--- src/main/java/com/partner/contract/category/dto/CategoryListResponseDto.java
+++ src/main/java/com/partner/contract/category/dto/CategoryListResponseDto.java
@@ -1,24 +1,28 @@
 package com.partner.contract.category.dto;
 
-import com.partner.contract.category.domain.Category;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
 public class CategoryListResponseDto {
     private Long id;
     private String name;
+    private Long countOfStandards;
+    private Long countOfAgreements;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime createdAt;
 
     @Builder
-    public CategoryListResponseDto(Long id, String name) {
+    public CategoryListResponseDto(Long id, String name, Long countOfStandards, Long countOfAgreements, LocalDateTime createdAt) {
         this.id = id;
         this.name = name;
-    }
-
-    public static CategoryListResponseDto fromEntity(Category category) {
-        return CategoryListResponseDto.builder()
-                .id(category.getId())
-                .name(category.getName())
-                .build();
+        this.countOfStandards = countOfStandards;
+        this.countOfAgreements = countOfAgreements;
+        this.createdAt = createdAt;
     }
 }
