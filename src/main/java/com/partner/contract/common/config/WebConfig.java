3ce82ddab612df5b4d7732cdb75bdf9fef058e89
DAY0522 <ekdud_____@naver.com>
2025-03-19 10:27:20 +0900

    :bug: cors 에러 처리가 제대로 동작하지 않는 현상 수정

--- src/main/java/com/partner/contract/common/config/WebConfig.java
+++ src/main/java/com/partner/contract/common/config/WebConfig.java
@@ -7,13 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${server.allowedOrigins}")
-    private String[] allowedOrigins;
+    @Value("${secret.cors.allowed-origins.local}")
+    private String local;
+
+    @Value("${secret.cors.allowed-origins.domain}")
+    private String domain;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
+                .allowedOrigins(local, domain)
                 .allowedMethods("GET", "POST", "PATCH", "DELETE");
     }
 }
