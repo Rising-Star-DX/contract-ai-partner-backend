b5601ac2711eb5db3e3ebd46e7decc5040ee50b7
minseokim0629 <han8121449@naver.com>
2025-03-19 11:45:09 +0900

    :sparkles: 전체 카테고리 api 구현 (#66)

--- src/main/java/com/partner/contract/category/service/CategoryService.java
+++ src/main/java/com/partner/contract/category/service/CategoryService.java
@@ -1,13 +1,24 @@
 package com.partner.contract.category.service;
 
+import com.partner.contract.category.dto.CategoryListResponseDto;
 import com.partner.contract.category.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+
+    public List<CategoryListResponseDto> findCategoryList() {
+        return categoryRepository.findAllByOrderByCreatedAt()
+                .stream()
+                .map(CategoryListResponseDto::fromEntity)
+                .collect(Collectors.toList());
+    }
 }
