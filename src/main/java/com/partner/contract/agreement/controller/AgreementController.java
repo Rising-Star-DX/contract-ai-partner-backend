cf107a002a4eec9069a53b8084a896a41bc6b86d
minseokim0629 <han8121449@naver.com>
2025-03-20 15:09:45 +0900

    :bug:  계약서 조회 API 하나로 합치기 (#75)

--- src/main/java/com/partner/contract/agreement/controller/AgreementController.java
+++ src/main/java/com/partner/contract/agreement/controller/AgreementController.java
@@ -2,13 +2,14 @@ package com.partner.contract.agreement.controller;
 
 import com.partner.contract.agreement.dto.AgreementListResponseDto;
 import com.partner.contract.agreement.service.AgreementService;
+import com.partner.contract.common.dto.FileUploadInitRequestDto;
 import com.partner.contract.global.exception.dto.SuccessResponse;
 import com.partner.contract.global.exception.error.SuccessCode;
-import com.partner.contract.common.dto.FileUploadInitRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 import java.util.Map;
 
@@ -20,25 +21,27 @@ public class AgreementController {
     private final AgreementService agreementService;
 
     @GetMapping
-    public ResponseEntity<SuccessResponse<List<AgreementListResponseDto>>> agreementList() {
-        List<AgreementListResponseDto> agreements = agreementService.findAgreementList();
+    public ResponseEntity<SuccessResponse<List<AgreementListResponseDto>>> agreementList(
+            @RequestParam(name = "name", required = false, defaultValue = "") String name,
+            @RequestParam(name = "category-id", required = false, defaultValue = "0") Long categoryId) {
+        List<AgreementListResponseDto> agreements = agreementService.findAgreementList(name, categoryId);
 
         return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SELECT_SUCCESS.getCode(), SuccessCode.SELECT_SUCCESS.getMessage(), agreements));
     }
 
-    @GetMapping(params = "name")
-    public ResponseEntity<SuccessResponse<List<AgreementListResponseDto>>> agreementListByName(@RequestParam("name") String name) {
-        List<AgreementListResponseDto> agreements = agreementService.findAgreementListByName(name);
-
-        return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SELECT_SUCCESS.getCode(), SuccessCode.SELECT_SUCCESS.getMessage(), agreements));
-    }
-
-    @GetMapping(params = "category-id")
-    public ResponseEntity<SuccessResponse<List<AgreementListResponseDto>>> agreementListByCategoryId(@RequestParam("category-id") Long categoryId) {
-        List<AgreementListResponseDto> agreements = agreementService.findAgreementListByCategoryId(categoryId);
-
-        return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SELECT_SUCCESS.getCode(), SuccessCode.SELECT_SUCCESS.getMessage(), agreements));
-    }
+//    @GetMapping(params = "name")
+//    public ResponseEntity<SuccessResponse<List<AgreementListResponseDto>>> agreementListByName(@RequestParam("name") String name) {
+//        List<AgreementListResponseDto> agreements = agreementService.findAgreementListByName(name);
+//
+//        return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SELECT_SUCCESS.getCode(), SuccessCode.SELECT_SUCCESS.getMessage(), agreements));
+//    }
+//
+//    @GetMapping(params = "category-id")
+//    public ResponseEntity<SuccessResponse<List<AgreementListResponseDto>>> agreementListByCategoryId(@RequestParam("category-id") Long categoryId) {
+//        List<AgreementListResponseDto> agreements = agreementService.findAgreementListByCategoryId(categoryId);
+//
+//        return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SELECT_SUCCESS.getCode(), SuccessCode.SELECT_SUCCESS.getMessage(), agreements));
+//    }
 
     @PostMapping("/upload/init")
     public ResponseEntity<SuccessResponse<Map<String, Long>>> agreementFileUploadInit(@RequestBody FileUploadInitRequestDto requestDto) {
