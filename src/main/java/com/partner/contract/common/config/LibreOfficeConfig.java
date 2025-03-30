e533367738db62023eae46389819c8ee6c5f80ee
minseokim0629 <han8121449@naver.com>
2025-03-30 19:00:04 +0900

    :construction:   Libreoffice 설정 주석 처리

--- src/main/java/com/partner/contract/common/config/LibreOfficeConfig.java
+++ src/main/java/com/partner/contract/common/config/LibreOfficeConfig.java
@@ -7,26 +7,28 @@ import jakarta.annotation.PreDestroy;
 import org.jodconverter.core.office.OfficeException;
 import org.jodconverter.core.office.OfficeManager;
 import org.jodconverter.local.office.LocalOfficeManager;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LibreOfficeConfig {
 
-    @Value("${libreoffice.home}")
-    private String libreofficeHome;
-
-    @Value("${libreoffice.port}")
-    private Integer libreofficePort;
+//    @Value("${libreoffice.home}")
+//    private String libreofficeHome;
+//
+//    @Value("${libreoffice.port}")
+//    private Integer libreofficePort;
 
     private OfficeManager officeManager;
 
     @Bean
     public OfficeManager officeManager() {
+//        officeManager = LocalOfficeManager.builder()
+//                .officeHome(libreofficeHome)
+//                .portNumbers(libreofficePort)
+//                .build();
         officeManager = LocalOfficeManager.builder()
-                .officeHome(libreofficeHome)
-                .portNumbers(libreofficePort)
+                .install()
                 .build();
         return officeManager;
     }
