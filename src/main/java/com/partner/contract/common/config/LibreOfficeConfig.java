6f527640f844d4619b3e5337e3260e762cc94c9e
minseokim0629 <han8121449@naver.com>
2025-03-30 21:28:07 +0900

    :fire: 불필요한 코드 제거

--- src/main/java/com/partner/contract/common/config/LibreOfficeConfig.java
+++ src/main/java/com/partner/contract/common/config/LibreOfficeConfig.java
@@ -10,8 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.util.Optional;
-
 @Configuration
 @Profile("ec2")
 public class LibreOfficeConfig {
@@ -19,43 +17,26 @@ public class LibreOfficeConfig {
     private OfficeManager officeManager;
 
     @Bean
-    public Optional<OfficeManager> officeManager() {
-        if (isLibreOfficeInstalled()) {
-            officeManager = LocalOfficeManager.builder().build();
-            try {
-                if (officeManager != null && !officeManager.isRunning()) {
-                    officeManager.start();
-                }
-            } catch (OfficeException e) {
-                throw new ApplicationException(ErrorCode.OFFICE_CONNECTION_ERROR);
+    public OfficeManager officeManager() {
+        officeManager = LocalOfficeManager.builder().build();
+        try {
+            if (!officeManager.isRunning()) {
+                officeManager.start();
             }
+        } catch (OfficeException e) {
+            throw new ApplicationException(ErrorCode.OFFICE_CONNECTION_ERROR);
         }
-        return Optional.ofNullable(officeManager);
+        return officeManager;
     }
 
     @PreDestroy
     public void destroy() {
         try {
-            if(isLibreOfficeInstalled()) {
-                if (officeManager != null && officeManager.isRunning()) {
-                    officeManager.stop();
-                }
+            if (officeManager.isRunning()) {
+                officeManager.stop();
             }
         } catch (OfficeException e) {
             throw new ApplicationException(ErrorCode.OFFICE_CONNECTION_ERROR);
-        }
-    }
-
-    private boolean isLibreOfficeInstalled() {
-        try {
-            // "libreoffice --version" 명령어를 실행하여 설치 여부를 확인
-            Process process = new ProcessBuilder("libreoffice", "--version").start();
-            int exitCode = process.waitFor();
-            System.out.println(exitCode);
-            return exitCode == 0; // 명령어 실행 성공 여부 (0이면 설치됨)
-        } catch (Exception e) {
-            System.err.println("LibreOffice installation check failed: " + e.getMessage());
-            return false; // 예외가 발생하면 설치되지 않은 것으로 간주
         }
     }
 }
