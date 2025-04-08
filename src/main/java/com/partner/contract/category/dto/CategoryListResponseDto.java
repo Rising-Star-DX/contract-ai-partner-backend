05d8a4a2e98083ace11d7f9ef9f0dc96d0b3fa11
minseokim0629 <han8121449@naver.com>
2025-04-08 18:21:44 +0900

    :bug: DTO 데이터 타입 변경

--- src/main/java/com/partner/contract/category/dto/CategoryListResponseDto.java
+++ src/main/java/com/partner/contract/category/dto/CategoryListResponseDto.java
@@ -1,32 +1,28 @@
 package com.partner.contract.category.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 @NoArgsConstructor
 public class CategoryListResponseDto {
-    private Integer id;
+    private Long id;
     private String name;
-    private Integer countOfStandards;
-    private Integer countOfAgreements;
-    private String createdAt;
+    private Long countOfStandards;
+    private Long countOfAgreements;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private Timestamp createdAt;
 
     @Builder
-    public CategoryListResponseDto(Integer id, String name, Integer countOfStandards, Integer countOfAgreements, Timestamp createdAt) {
+    public CategoryListResponseDto(Long id, String name, Long countOfStandards, Long countOfAgreements, Timestamp createdAt) {
         this.id = id;
         this.name = name;
         this.countOfStandards = countOfStandards;
         this.countOfAgreements = countOfAgreements;
-        this.createdAt = convertTimestampToLocalDateTime(createdAt);
-    }
-
-    private String convertTimestampToLocalDateTime(Timestamp timestamp) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
-        return timestamp.toLocalDateTime().format(formatter);
+        this.createdAt = createdAt;
     }
 }
