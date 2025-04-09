57a73dd1cc92362c31732fcae649abccfec618a2
minseokim0629 <han8121449@naver.com>
2025-04-09 13:58:08 +0900

    :bug: DTO 생성날짜 제거

--- src/main/java/com/partner/contract/category/dto/CategoryNameListResponseDto.java
+++ src/main/java/com/partner/contract/category/dto/CategoryNameListResponseDto.java
@@ -1,33 +1,26 @@
 package com.partner.contract.category.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.partner.contract.category.domain.Category;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.sql.Timestamp;
 
 @Getter
 @NoArgsConstructor
 public class CategoryNameListResponseDto {
     private Long id;
     private String name;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
-    private Timestamp createdAt;
 
     @Builder
-    public CategoryNameListResponseDto(Long id, String name, Timestamp createdAt) {
+    public CategoryNameListResponseDto(Long id, String name) {
         this.id = id;
         this.name = name;
-        this.createdAt = createdAt;
     }
 
     public static CategoryNameListResponseDto fromEntity(Category category) {
         return CategoryNameListResponseDto.builder()
                 .id(category.getId())
                 .name(category.getName())
-                .createdAt(Timestamp.valueOf(category.getCreatedAt()))
                 .build();
     }
 }
