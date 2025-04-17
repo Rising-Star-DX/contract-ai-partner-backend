be19848bd902bcc091ff21c113c5d138bb5ba03e
minseokim0629 <han8121449@naver.com>
2025-04-17 10:16:49 +0900

    :sparkles: 기준문서 수정된 텍스트를 txt로 변환 (#123)

--- src/main/java/com/partner/contract/common/service/FileConversionService.java
+++ src/main/java/com/partner/contract/common/service/FileConversionService.java
@@ -6,13 +6,16 @@ import com.partner.contract.global.exception.error.ApplicationException;
 import com.partner.contract.global.exception.error.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -72,14 +75,38 @@ public class FileConversionService {
         } catch (IOException | InterruptedException e) {
             throw new ApplicationException(ErrorCode.FILE_PROCESSING_ERROR);
         } finally {
-            if (tempInputFile != null && tempInputFile.exists()) tempInputFile.delete();
-            if (outputPdfFile != null && outputPdfFile.exists()) outputPdfFile.delete();
+            //if (tempInputFile != null && tempInputFile.exists()) tempInputFile.delete();
+            //if (outputPdfFile != null && outputPdfFile.exists()) outputPdfFile.delete();
         }
     }
 
     private boolean isLibreOfficeAvailable() {
         File libreOffice = new File(libreOfficePath);
         return libreOffice.exists() && libreOffice.canExecute();
+    }
+
+    public MultipartFile convertStringToTxt(String name, List<String> contents) {
+        try {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < contents.size(); i++) {
+                sb.append(contents.get(i));
+                if (i < contents.size() - 1) {
+                    sb.append("\f"); // 페이지별 구분을 위해 \f(Form Feed) 문자 사용
+                }
+            }
+
+            byte[] contentBytes = sb.toString().getBytes("UTF-8");
+
+            return new MockMultipartFile(
+                    "file",
+                    name + ".txt",
+                    "text/plain",
+                    new ByteArrayInputStream(contentBytes)
+            );
+
+        } catch (IOException e) {
+            throw new ApplicationException(ErrorCode.FILE_PROCESSING_ERROR);
+        }
     }
 }
 
