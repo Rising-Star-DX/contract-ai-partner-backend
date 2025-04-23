753f53bd9eed95c50481d4c992e3bd0ce6629b67
minseokim0629 <han8121449@naver.com>
2025-04-24 00:11:54 +0900

    :sparkles: kafka producer 코드 추가 (#133)

--- src/main/java/com/partner/contract/common/service/KafkaProducerService.java
+++ src/main/java/com/partner/contract/common/service/KafkaProducerService.java
@@ -1,6 +1,7 @@
 package com.partner.contract.common.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +9,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KafkaProducerService {
     private final KafkaTemplate<String, Object> kafkaTemplate;
-    private static final String TOPIC = "analysis_topic";
 
-    public <T> void sendMessage(T message) {
-        kafkaTemplate.send(TOPIC, message);
+    @Value("${kafka.topics.standard-analysis-request}")
+    private String standardAnalysisRequestTopic;
+    @Value("${kafka.topics.agreement-analysis-request}")
+    private String agreementAnalysisRequestTopic;
+
+    public <T> void sendStandardAnalysisRequest(T message) {
+        kafkaTemplate.send(standardAnalysisRequestTopic, message);
+    }
+
+    public <T> void sendAgreementAnalysisRequest(T message) {
+        kafkaTemplate.send(agreementAnalysisRequestTopic, message);
     }
 }
