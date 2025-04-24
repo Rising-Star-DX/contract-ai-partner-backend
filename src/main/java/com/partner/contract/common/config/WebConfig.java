5b83d5d8289d52ab656ac82260deb2a03fa41669
minseokim0629 <han8121449@naver.com>
2025-04-24 17:40:11 +0900

    :bug:  모든 도메인에서 cors 임시 허용

--- src/main/java/com/partner/contract/common/config/WebConfig.java
+++ src/main/java/com/partner/contract/common/config/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(local, domain)
+                //.allowedOrigins(local, domain)
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE");
     }
 }
