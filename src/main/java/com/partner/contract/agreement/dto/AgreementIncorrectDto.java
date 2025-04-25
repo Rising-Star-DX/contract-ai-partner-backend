a177be657c7affc05211a26673b3500e9dc1b8bc
minseokim0629 <han8121449@naver.com>
2025-04-25 11:13:43 +0900

    :sparkles: 생성자 어노테이션 추가(#133)

--- src/main/java/com/partner/contract/agreement/dto/AgreementIncorrectDto.java
+++ src/main/java/com/partner/contract/agreement/dto/AgreementIncorrectDto.java
@@ -2,11 +2,13 @@ package com.partner.contract.agreement.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 
 @Data
+@NoArgsConstructor
 public class AgreementIncorrectDto {
     private String incorrectText;
     private String proofText;
