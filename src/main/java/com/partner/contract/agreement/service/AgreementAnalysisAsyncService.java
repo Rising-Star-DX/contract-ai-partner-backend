6d8726ea9903f8eb8ce02aaf04a5415adf45792e
minseokim0629 <han8121449@naver.com>
2025-04-25 11:13:08 +0900

    :sparkles: 카프카 메서드 추가(#133)

--- src/main/java/com/partner/contract/agreement/service/AgreementAnalysisAsyncService.java
+++ src/main/java/com/partner/contract/agreement/service/AgreementAnalysisAsyncService.java
@@ -37,8 +37,8 @@ public class AgreementAnalysisAsyncService {
     private final AgreementRepository agreementRepository;
     private final AgreementIncorrectTextRepository agreementIncorrectTextRepository;
     private final AgreementIncorrectPositionRepository agreementIncorrectPositionRepository;
-    private final KafkaProducerService kafkaProducerService;
     private final RestTemplate restTemplate;
+    private final KafkaProducerService kafkaProducerService;
 
     @Value("${secret.flask.ip}")
     private String FLASK_SERVER_IP;
@@ -46,9 +46,9 @@ public class AgreementAnalysisAsyncService {
     @Async
     @Transactional(noRollbackFor = ApplicationException.class)
     public void analyze(Agreement agreement, String categoryName){
-        //try {
-            // Flask에 AI 분석 요청
-            //String url = FLASK_SERVER_IP + "/flask/agreements/analysis";
+        try {
+             //Flask에 AI 분석 요청
+            String url = FLASK_SERVER_IP + "/flask/agreements/analysis";
 
             AnalysisRequestDto analysisRequestDto = AnalysisRequestDto.builder()
                     .id(agreement.getId())
@@ -58,76 +58,95 @@ public class AgreementAnalysisAsyncService {
                     .build();
 
             // HTTP Request Header 설정
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//            // HTTP Request Body 설정
-//            HttpEntity<AnalysisRequestDto> requestEntity = new HttpEntity<>(analysisRequestDto, headers);
-//
-//            FlaskResponseDto<AgreementAnalysisFlaskResponseDto> body = null;
-            try {
-//                // Flask에 API 요청
-//                ResponseEntity<FlaskResponseDto<AgreementAnalysisFlaskResponseDto>> response = restTemplate.exchange(
-//                        url,
-//                        HttpMethod.POST,
-//                        requestEntity,
-//                        new ParameterizedTypeReference<FlaskResponseDto<AgreementAnalysisFlaskResponseDto>>() {} // ✅ 제네릭 타입 유지
-//                );
-//
-//                body = response.getBody();
-            kafkaProducerService.sendAgreementAnalysisRequest(analysisRequestDto);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
 
-            } catch (KafkaException e) {
+            // HTTP Request Body 설정
+            HttpEntity<AnalysisRequestDto> requestEntity = new HttpEntity<>(analysisRequestDto, headers);
+
+            FlaskResponseDto<AgreementAnalysisFlaskResponseDto> body = null;
+            try {
+                // Flask에 API 요청
+                ResponseEntity<FlaskResponseDto<AgreementAnalysisFlaskResponseDto>> response = restTemplate.exchange(
+                        url,
+                        HttpMethod.POST,
+                        requestEntity,
+                        new ParameterizedTypeReference<FlaskResponseDto<AgreementAnalysisFlaskResponseDto>>() {} // ✅ 제네릭 타입 유지
+                );
+
+                body = response.getBody();
+
+            } catch (RestClientException e) {
                 agreement.updateAiStatus(AiStatus.FAILED);
                 agreementRepository.save(agreement);
-                throw new ApplicationException(ErrorCode.KAFKA_SERVER_CONNECTION_ERROR);
+                throw new ApplicationException(ErrorCode.FLASK_SERVER_CONNECTION_ERROR);
             }
 
             // Flask에서 넘어온 계약서 정보 data
-//            AgreementAnalysisFlaskResponseDto flaskResponseDto = body.getData();
-//            List<AgreementIncorrectDto> agreementIncorrectDtos = flaskResponseDto.getAgreementIncorrectDtos();
-//
-//            for (AgreementIncorrectDto agreementIncorrectDto : agreementIncorrectDtos) {
-//                AgreementIncorrectText agreementIncorrectText = AgreementIncorrectText.builder()
-//                        .accuracy(agreementIncorrectDto.getAccuracy() * 100)
-//                        .incorrectText(agreementIncorrectDto.getIncorrectText())
-//                        .proofText(agreementIncorrectDto.getProofText())
-//                        .correctedText(agreementIncorrectDto.getCorrectedText())
-//                        .agreement(agreement)
-//                        .build();
-//
-//                agreementIncorrectTextRepository.save(agreementIncorrectText);
-//
-//                for(IncorrectClauseDataDto incorrectClauseDataDto : agreementIncorrectDto.getIncorrectClauseDataDtoList()) {
-//                    if (incorrectClauseDataDto.getPosition() == null || incorrectClauseDataDto.getPosition().isEmpty() || incorrectClauseDataDto.getPositionPart() == null) {
-//                        agreement.updateAiStatus(AiStatus.FAILED);
-//                        agreementRepository.save(agreement);
-//                        throw new ApplicationException(ErrorCode.AI_ANALYSIS_POSITION_EMPTY_ERROR);
-//                    }
-//
-//                    AgreementIncorrectPosition agreementIncorrectPosition = AgreementIncorrectPosition.builder()
-//                            .position(incorrectClauseDataDto.getPosition().toString())
-//                            .positionPart(incorrectClauseDataDto.getPositionPart().toString())
-//                            .page(incorrectClauseDataDto.getPage())
-//                            .orderIndex(incorrectClauseDataDto.getOrderIndex())
-//                            .agreementIncorrectText(agreementIncorrectText)
-//                            .build();
-//
-//                    agreementIncorrectPositionRepository.save(agreementIncorrectPosition);
-//                }
-//            }
-//
-//            // AI 상태 및 분석 정보 업데이트
-//            agreement.updateAiStatus(AiStatus.SUCCESS);
-//            agreement.updateAnalysisInfomation(flaskResponseDto.getTotalPage(), flaskResponseDto.getTotalChunks());
-//            agreementRepository.save(agreement);
-//
-//        } catch (ApplicationException e) {
-//            throw e;
-//        } catch (Exception e) {
-//            agreement.updateAiStatus(AiStatus.FAILED);
-//            agreementRepository.save(agreement);
-//            throw new ApplicationException(ErrorCode.FLASK_ANALYSIS_ERROR);
-//        }
+            AgreementAnalysisFlaskResponseDto flaskResponseDto = body.getData();
+            List<AgreementIncorrectDto> agreementIncorrectDtos = flaskResponseDto.getAgreementIncorrectDtos();
+
+            for (AgreementIncorrectDto agreementIncorrectDto : agreementIncorrectDtos) {
+                AgreementIncorrectText agreementIncorrectText = AgreementIncorrectText.builder()
+                        .accuracy(agreementIncorrectDto.getAccuracy() * 100)
+                        .incorrectText(agreementIncorrectDto.getIncorrectText())
+                        .proofText(agreementIncorrectDto.getProofText())
+                        .correctedText(agreementIncorrectDto.getCorrectedText())
+                        .agreement(agreement)
+                        .build();
+
+                agreementIncorrectTextRepository.save(agreementIncorrectText);
+
+                for(IncorrectClauseDataDto incorrectClauseDataDto : agreementIncorrectDto.getIncorrectClauseDataDtoList()) {
+                    if (incorrectClauseDataDto.getPosition() == null || incorrectClauseDataDto.getPosition().isEmpty() || incorrectClauseDataDto.getPositionPart() == null) {
+                        agreement.updateAiStatus(AiStatus.FAILED);
+                        agreementRepository.save(agreement);
+                        throw new ApplicationException(ErrorCode.AI_ANALYSIS_POSITION_EMPTY_ERROR);
+                    }
+
+                    AgreementIncorrectPosition agreementIncorrectPosition = AgreementIncorrectPosition.builder()
+                            .position(incorrectClauseDataDto.getPosition().toString())
+                            .positionPart(incorrectClauseDataDto.getPositionPart().toString())
+                            .page(incorrectClauseDataDto.getPage())
+                            .orderIndex(incorrectClauseDataDto.getOrderIndex())
+                            .agreementIncorrectText(agreementIncorrectText)
+                            .build();
+
+                    agreementIncorrectPositionRepository.save(agreementIncorrectPosition);
+                }
+            }
+
+            // AI 상태 및 분석 정보 업데이트
+            agreement.updateAiStatus(AiStatus.SUCCESS);
+            agreement.updateAnalysisInfomation(flaskResponseDto.getTotalPage(), flaskResponseDto.getTotalChunks());
+            agreementRepository.save(agreement);
+
+        } catch (ApplicationException e) {
+            throw e;
+        } catch (Exception e) {
+            agreement.updateAiStatus(AiStatus.FAILED);
+            agreementRepository.save(agreement);
+            throw new ApplicationException(ErrorCode.FLASK_ANALYSIS_ERROR);
+        }
+    }
+
+    @Async
+    @Transactional(noRollbackFor = ApplicationException.class)
+    public void analyzeWithKafka(Agreement agreement, String categoryName){
+
+        AnalysisRequestDto analysisRequestDto = AnalysisRequestDto.builder()
+                .id(agreement.getId())
+                .url(agreement.getUrl())
+                .categoryName(categoryName)
+                .type(agreement.getType())
+                .build();
+
+        try {
+            kafkaProducerService.sendAgreementAnalysisRequest(analysisRequestDto);
+        } catch (KafkaException e) {
+            agreement.updateAiStatus(AiStatus.FAILED);
+            agreementRepository.save(agreement);
+            throw new ApplicationException(ErrorCode.KAFKA_SERVER_CONNECTION_ERROR);
+        }
     }
 }
